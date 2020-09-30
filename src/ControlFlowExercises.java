be6cf33b5da4cc6bebc8e64f45e3a829e1c3dda6
Unknown <blossomperez18@gmail.com>
2020-09-30 12:51:04 -0500

    Complete loops practice

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,56 @@
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        // int w = 5;
+        // while(w <= 15) {
+        //     System.out.println(w);
+        //     w++;
+        // }
+        
+        // for(int i = 5; i <= 15; i++) {
+        //     System.out.printf("%d ", i);
+        // }
+
+        // int y = 2;
+        // do{
+        //     System.out.println(y);
+        //     y+=2;
+        // }while(y <= 100);
+
+        // for(int i = 0; i <= 100; i+=2) {
+        //         System.out.printf("%d%n", i);
+            
+        // }
+
+        // int z = 100;
+        // do{
+        //     System.out.println(z);
+        //     z -= 5;
+        // }while(z >= 5);
+
+        // for(int i = 100; i >= 5; i-=5) {
+        //     System.out.printf("%s%n", i);
+        // }
+
+        // long n = 2;
+        // do{
+        //     System.out.println(n);
+        //     n *= n;
+        // }while(n <=100000);
+
+        // for(int i = 1; i <= 100; i++) {
+        //     if(i % 15 == 0) {
+        //         System.out.println("fizzbuzz");
+        //     }else if(i % 5 == 0) {
+        //         System.out.println("buzz");
+        //     }else if(i % 3 == 0) {
+        //         System.out.println("fizz");
+        //     }else{
+        //         System.out.println(i);
+        //     }
+        // }
+
+
+        
+    }
+}
