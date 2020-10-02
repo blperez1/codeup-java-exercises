4cb8280c2bc3d14d07354f62d2e5b07783a0471b
Unknown <blossomperez18@gmail.com>
2020-10-02 16:34:19 -0500

    Refactor string exercise

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -2,7 +2,7 @@ public class StringExercise {
     public static void main(String[] args) {
         String str1 = "We don't need no education";
         String str2 = "We don't need no thougth control";
-
+        
         String str3 = "Check \"this\" out!, \"s inside of \"s!";
         String str4 = "In windows, the main drive is usually C:\\";
         String str5 = "I can do backslashes \\, double backslashes \\\\,";
