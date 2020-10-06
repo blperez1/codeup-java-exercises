7c30f2fc0e745182e34112c9291cadb085267c52
Unknown <blossomperez18@gmail.com>
2020-10-06 15:51:28 -0500

    Complete circle app

---
+++ src/shapes/CircleApp.java
@@ -0,0 +1,12 @@
+package shapes;
+import util.Input;
+public class CircleApp {
+    public static void main(String[] args) {
+        Input input = new Input();
+        double rad = input.getDouble(0, 100000);
+        Circle pokerChip = new Circle(rad);
+        System.out.println(pokerChip.getCircumference());
+        System.out.println(pokerChip.getArea());
+
+    }
+}
