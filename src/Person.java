099beea641a55c754084588ef689ab41510107ae
Unknown <blossomperez18@gmail.com>
2020-10-06 12:26:58 -0500

    Add a constructor

--- src/Person.java
+++ src/Person.java
@@ -1,23 +1,19 @@
 public class Person {
     public static void main(String[] args) {
     //The strings are the same but the objects are not.
-        Person person1 = new Person();
-        Person person2 = new Person();
-        person1.name = "John";
-        person2.name = "John";
+        Person person1 = new Person("John");
+        Person person2 = new Person("John");
         System.out.println(person1.getName().equals(person2.getName()));
         System.out.println(person1 == person2);
 
     //Person 3 and 4 are seen as the same object
-        Person person3 = new Person();
+        Person person3 = new Person("John");
         Person person4 = person3;
-        person3.name = "John";
         System.out.println(person3 == person4);
 
     //
-        Person person5 = new Person();
+        Person person5 = new Person("John");
         Person person6 = person5;
-        person5.name = "John";
         System.out.println(person5.getName());
         System.out.println(person6.getName());
         person6.setName("Jane");
@@ -27,6 +23,10 @@ public class Person {
     }
 
     private String name; 
+
+    public Person(String name) {
+        this.name = name;
+    }
 
     public String getName() {
         return name; 
