5d19f2b5e42641c79081f3ea67153ca53d2ee77f
Jason Gutierrez <jasong84@gmail.com>
2020-10-08 21:43:46 -0500

    Add comment to Square class

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,7 +2,7 @@ package shapes;
 
 public class Square extends Rectangle {
     private double side;
-    
+    // The constructor
     public Square(double side) {
         super(side, side);
         this.side = side;
