06d9e4d309e69c682626eca0a7e482a92865eaef
Unknown <blossomperez18@gmail.com>
2020-10-11 12:40:30 -0500

    Create Student command line interface

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,5 +1,6 @@
 package grades;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class GradesApplication {
     public static void main(String[] args) {
@@ -25,7 +26,31 @@ public class GradesApplication {
         bryan.addGrade(80);
         bryan.addGrade(100);
 
-        HashMap<String, String> usernames = new HashMap<>();
+        HashMap<Student, String> Students = new HashMap<>();
+        Scanner sc = new Scanner(System.in);
 
+        Students.put(isaiah, "collgate123");
+        Students.put(isabel, "peachesUnicorn");
+        Students.put(marcelene, "mjDesigns");
+        Students.put(bryan, "Anthony23");
+
+        System.out.println("Welcome");
+        System.out.println("Here are the GitHub usernames of our students");
+        System.out.printf("|%s| |%s| |%s| |%s|", 
+                        Students.get(isabel), 
+                        Students.get(isaiah), 
+                        Students.get(marcelene),
+                        Students.get(bryan));
+        System.out.println("What student do you want to see more information on?");
+        String input = sc.next();
+
+        if(!Students.containsValue(input)) {
+            System.out.printf("Sorry, no student found with the GitHub username of \"%s\".", input);
+            System.out.println("Would you like to see another student?");
+        }
+        }
+
+        
+        
     }
-}
+
