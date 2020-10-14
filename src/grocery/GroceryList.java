827e62e6d6da37fbf9e2988321ae0e6cf42edcec
Unknown <blossomperez18@gmail.com>
2020-10-14 09:59:54 -0500

    Create GroceryList class to test Grocery class

---
+++ src/grocery/GroceryList.java
@@ -0,0 +1,13 @@
+package grocery;
+import util.Input;
+
+public class GroceryList {
+    public static void main(String[] args) {
+        Input input = new Input();
+        
+
+        
+    }
+
+
+}
