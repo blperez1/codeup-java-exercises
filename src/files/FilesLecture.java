6cc63926e62cd845efc387d09a1f7f30197a118d
Unknown <blossomperez18@gmail.com>
2020-10-16 18:02:21 -0500

    Refactor repeated code into methods in FileIO

--- src/files/FilesLecture.java
+++ src/files/FilesLecture.java
@@ -10,66 +10,39 @@ import java.util.Arrays;
 import java.util.List;
 
 public class FilesLecture {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String directoryName = "data";
         String fileName = "grocery_list.txt";
 
-        Path directoryPath = Paths.get(directoryName);
-        Path dataFilePath = Paths.get(directoryName, fileName);
+        //We have to create a directory first before we create before we create the file.
+        try {
+            Path dataFilePath = FileIO.createDirectoryAndFile(directoryName, fileName);
 
-        //Create a direcgtory first before creating file
-        try{
-            if(Files.notExists(directoryPath)) {
-                Files.createDirectories(directoryPath);
-            }
-            if(!Files.exists(dataFilePath)) {
-                Files.createFile(dataFilePath);
-            }
-            List<String> grocerList = Arrays.asList("milk", "eggs", "bacon");
-            Files.write(dataFilePath, grocerList);
+            //Let's write our grocery list ot the file.
+            List<String> groceryList = Arrays.asList("milk", "eggs", "bacon");
+            Files.write(dataFilePath, groceryList);
 
-            List<String> fileContents = Files.readAllLines(dataFilePath);
-            for(int i = 0; i < fileContents.size(); i++) {
-                System.out.printf("%d: %s%n", i + 1, fileContents.get(i));
-            }
+            FileIO.printFileContents(dataFilePath);
 
-            //Append to the file
-            Files.write(dataFilePath,Arrays.asList("cereal", "bread"), StandardOpenOption.APPEND);
-            fileContents = Files.readAllLines(dataFilePath);
-            System.out.println();
-            for(int i = 0; i < fileContents.size(); i++) {
-                System.out.printf("%d: %s%n", i + 1, fileContents.get(i));
-            }
+            //Append to the file.
+            Files.write(dataFilePath, Arrays.asList("cereal", "bread"), StandardOpenOption.APPEND);
+            FileIO.printFileContents(dataFilePath);
 
             //Replace a line in the file.
-            fileContents = Files.readAllLines(dataFilePath);
-            List<String> modifiedList = new ArrayList<>();
-            System.out.println();
-            for(String item: fileContents) {
-                if(item.equals("milk")) {
-                    modifiedList.add("whole milk");
-                }else{
-                    modifiedList.add(item);
-                }
-            }
+            FileIO.updateLine(dataFilePath, "milk", "whole milk");
+            FileIO.printFileContents(dataFilePath);
 
-            //Remove a line from file
-            fileContents = Files.readAllLines(dataFilePath);
-            System.out.println();
-            modifiedList = new ArrayList<>();
+            //Remove a line from the file.
+            FileIO.deleteLine(dataFilePath, "bread");
+            FileIO.printFileContents(dataFilePath);
 
-            for(String item: fileContents) {
-                if(!item.equals("bread")) {
-                    modifiedList.add(item);
-                }
-            }
-            Files.write(dataFilePath, modifiedList);
-            for(int i = 0; i < fileContents.size(); i++) {
-                System.out.printf("%d: %s%n", i + 1, fileContents.get(i));
-            }
+            //Empty the list
+            Files.write(dataFilePath, new ArrayList<>());
+            System.out.println("After empty");
+            FileIO.printFileContents(dataFilePath);
 
-        }catch(IOException ex) {
-            System.out.println("cannot create the file.");
+        } catch (IOException ex) {
+            System.out.println("Cannot create the file.");
             ex.printStackTrace();
         }
 
